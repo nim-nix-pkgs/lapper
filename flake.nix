c4812c0ef347fbbddbbaf02fbdeee830f6390bcd
hugosenari <hugosenari@gmail.com>
2022-03-12 02:51:03 -0300

    chore: re index lapper

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = ''fast interval overlaps'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."lapper-master".url = "path:./master";
+  inputs."lapper-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."lapper-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."lapper-v0_1_7".url = "path:./v0_1_7";
+  inputs."lapper-v0_1_7".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."lapper-v0_1_7".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''fast interval overlaps'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-lapper-master.flake = false;
+  inputs.src-lapper-master.owner = "brentp";
+  inputs.src-lapper-master.ref   = "refs/heads/master";
+  inputs.src-lapper-master.repo  = "nim-lapper";
+  inputs.src-lapper-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-lapper-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-lapper-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
